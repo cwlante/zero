cd2afc15336692c64cad7fbc46c524bcef17bab3
cwlante <48371349+cwlante@users.noreply.github.com>
2023-10-15 14:07:31 +0800

    Update a009.cpp

--- a009.cpp
+++ a009.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     int k = -7;
-    string sec;
-    getline(cin, sec);
-    string cle(sec.size(),' ');
+    string sec;   //宣告名為sec的字串
+    getline(cin, sec);    //將整行輸入指派為sec
+    string cle(sec.size(),' ');    //宣告名為cle的字串並給予大小與sec相等的空格
     for(int i = 0; i < sec.size(); i++){
         cle[i] = sec[i]+k;
-    }
+    }    //重複執行將祕文位移k位並指派到cle
     cout << cle << /n;
 }
