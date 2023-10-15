b116f365b850756464c0ea7f301461dff9e9b8cb
cwlante <48371349+cwlante@users.noreply.github.com>
2023-10-15 20:50:59 +0800

    Create a005.cpp

---
+++ a005.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int t;
+  for(int i;i << t; i++){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if(b-a == c-b == d=c){
+      int D=b-a;
+      cout >> a >> b >> c >> d >> d+D;
+    }
+    else{
+      int R=b/a;
+      for()
+  }
+}
