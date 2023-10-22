a5c8bc36d97642bb76920403d26cc8f377ba0a54
cwlante <48371349+cwlante@users.noreply.github.com>
2023-10-22 23:09:17 +0800

    Update a005.cpp

--- a005.cpp
+++ a005.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
  int t;
-  for(int i;i << t; i++){
+ cin >> t;
+  for(int i;i < t; i++){
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    if(b-a == c-b == d=c){
+    if(b-a == c-b && c-b == d-c){
       int D=b-a;
-      cout >> a >> b >> c >> d >> d+D;
+      cout << a << " " << b << " " << c << " " << d << " " << d+D << "\n";
     }
     else{
       int R=b/a;
-      for()
+     cout << a << " ";
+      for(int n=1;n <= 3;n++){
+       cout << a*pow(R,n) << " ";
+      }
+      cout << a*pow(R,4) << endl;
+   }
   }
 }
