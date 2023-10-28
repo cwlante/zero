146caa86a5d3e464e3c1705b3b87a9f1b6ffb3b5
cwlante <48371349+cwlante@users.noreply.github.com>
2023-10-28 20:13:11 +0800

    Update a010.cpp

--- a010.cpp
+++ a010.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int n;
-  cin >> n;
-  
+  int n, x=2, s=0, c=0;
+  while(cin >> n){
+    while(x<=n){
+      if(n%x == 0){
+        if(x==s){
+          c++;
+          n=n/x;
+          x=2;
+          continue;
+        }
+        else{ 
+          if(c!=0){
+          cout << "^" << c;
+          c=0;
+          n=n/x;
+          s=x;
+          x=2;
+          continue;
+        }
+        else if(s!=0)
+          cout << " * ";
+        cout << x;
+        n=n/x;
+        s=x;
+        x=2;
+      }
+      else
+        x++;
+    }
+    if (n==1)
+      break;
+  }
 }
-
