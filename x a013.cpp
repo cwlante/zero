365e08b157e411c66ea2ac5e8f381ba7134a9b55
cwlante <48371349+cwlante@users.noreply.github.com>
2023-10-28 20:50:48 +0800

    Create x a013.cpp

---
+++ x a013.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+  string A, B;
+  int a,b, n;
+  while(cin >> A >> B){
+    if(A=="#" || b=="#")
+      break;
+    n=sqrt(pow(a-b,2));
+  }
+}
+
+int RtoN(string i){
+    int x=0, I=1, V=5, X=10, L=50, C=100, D=500, M=1000;
+    if 
+  }
